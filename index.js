e53b2308a4e4938d1b15dee4974d7742bfe343ce
elraphty <elraphtyofficial@gmail.com>
2019-04-03 22:18:42 +0000

    set view path and view engine

--- index.js
+++ index.js
@@ -3,33 +3,46 @@ const cors = require('cors');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const puppeteer = require('puppeteer');
+const path = require('path');
 
 // Load env values
 require('dotenv').config()
 
+const PORT = process.env.PORT;
+
+// ROUTE FILES
+
+const indexRoutes = require('./routes/index');
+
 const app = express();
+
+// Routes declaration 
+app.use('/', indexRoutes);
 
 app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+// set public path
+app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = process.env.PORT;
-
-
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  // await page.setViewport({width: 1000, height: 500});
-  await page.goto('http://tssdevs.com');
-  await page.screenshot({path: 'example.png'});
+// set view engine
+app.set('view engine', 'ejs');
 
 
-  // let html = await page.content();
+// (async () => {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   // await page.setViewport({width: 1000, height: 500});
+//   await page.goto('http://tssdevs.com');
+//   await page.screenshot({path: 'example.png'});
 
-  await browser.close();
-})();
+
+//   // let html = await page.content();
+
+//   await browser.close();
+// })();
 
 app.listen(PORT, () => {
   console.log('\x1b[33m%s\x1b[0m', `listening of port ${PORT}`)
