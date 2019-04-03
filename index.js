38c64b5e239d2cacf8bcde9197d479d54e7a2b0a
elraphty <elraphtyofficial@gmail.com>
2019-04-03 21:55:24 +0000

    setting up expressjs

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 const puppeteer = require('puppeteer');
+
+// Load env values
+require('dotenv').config()
+
+const app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+const PORT = process.env.PORT;
+
 
 (async () => {
   const browser = await puppeteer.launch();
@@ -12,3 +30,7 @@ const puppeteer = require('puppeteer');
 
   await browser.close();
 })();
+
+app.listen(PORT, () => {
+  console.log('\x1b[33m%s\x1b[0m', `listening of port ${PORT}`)
+});
